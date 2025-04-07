179b4ef3c7edaa3ae6712b8adde28c5eb7f0f9af
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-04-07 15:39:40 +0200

    Fix tests

--- C/main.c
+++ C/main.c
@@ -5,7 +5,7 @@ Creator: Claudio Raimondi
 Email: claudio.raimondi@pm.me                                                   
 
 created at: 2025-04-04 16:42:53                                                 
-last edited: 2025-04-07 15:30:17                                                
+last edited: 2025-04-07 15:39:40                                                
 
 ================================================================================*/
 
@@ -18,7 +18,10 @@ inline static bool m_isspace(const char c);
 static size_t m_strlen(const char *s);
 static bool m_strncmp(const char *s1, const char *s2, size_t n);
 
+int tests_run = 0;
+
 #define RUN_TEST(input, expected) \
+  printf("running test %d\n", tests_run); \ 
   run(&head, &tail, input); \
   head = tail = NULL;
 
@@ -27,200 +30,278 @@ int main(void)
   t_node *head = NULL;
   t_node *tail = NULL;
 
-  char *input = NULL;
-  char *expected = NULL;
-
   //Parsing - two adjacent separators
-  input = "~~";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "~~";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Parsing - three adjacent separators
-  input = "~~~";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "~~~";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Parsing - separator at end
-  input = "ADD(x)~";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x)~";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Add - malformed ADD
-  input = "ADDD(x) ~ AADD(x) ~ AD D(x) ~ ADD (x) ~ ADD() ~ ADD ~ PRINT";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADDD(x) ~ AADD(x) ~ AD D(x) ~ ADD (x) ~ ADD() ~ ADD ~ PRINT";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Add - add '\0'
-  input = "ADD('\0') ~ PRINT";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD('\0') ~ PRINT";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Add - add two chars in same command
-  input = "ADD(ab) ~ PRINT";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(ab) ~ PRINT";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Add - add one
-  input = "ADD(a) ~ PRINT";
-  expected = "a";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(a) ~ PRINT";
+    char expected[] = "a";
+    RUN_TEST(input, expected);
+  }
 
   //Add - add same element twice
-  input = "ADD(a) ~ ADD(a) ~ PRINT";
-  expected = "aa";
+  {
+    char input[] = "ADD(a) ~ ADD(a) ~ PRINT";
+    char expected[] = "aa";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - malformed SORT
-  input = "ADD(2) ~ ADD(1) ~ SORTT ~ S ORT ~ SORT() ~ SSORT ~ PRINT";
-  expected = "21";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(2) ~ ADD(1) ~ SORTT ~ S ORT ~ SORT() ~ SSORT ~ PRINT";
+    char expected[] = "21";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - normal
-  input = "ADD(2) ~ ADD(1) ~ SORT ~ PRINT";
-  expected = "12";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(2) ~ ADD(1) ~ SORT ~ PRINT";
+    char expected[] = "12";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - empty
-  input = "SORT ~ PRINT";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "SORT ~ PRINT";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - one element
-  input = "ADD(1) ~ SORT ~ PRINT";
-  expected = "1";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(1) ~ SORT ~ PRINT";
+    char expected[] = "1";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - odd # of elements
-  input = "ADD(3) ~ ADD(1) ~ ADD(2) ~ SORT ~ PRINT";
-  expected = "123";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(3) ~ ADD(1) ~ ADD(2) ~ SORT ~ PRINT";
+    char expected[] = "123";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - even # of elements
-  input = "ADD(4) ~ ADD(2) ~ ADD(3) ~ ADD(1) ~ SORT ~ PRINT";
-  expected = "1234";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(4) ~ ADD(2) ~ ADD(3) ~ ADD(1) ~ SORT ~ PRINT";
+    char expected[] = "1234";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - already sorted
-  input = "ADD(1) ~ ADD(2) ~ ADD(3) ~ ADD(4) ~ SORT ~ PRINT";
-  expected = "1234";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(1) ~ ADD(2) ~ ADD(3) ~ ADD(4) ~ SORT ~ PRINT";
+    char expected[] = "1234";
+    RUN_TEST(input, expected);
+  }
 
   //Delete - malformed DELETE
-  input = "ADD(x) DE L(x) ~ DELL(x) ~ DDEL(x) ~ DEL() ~ DEL ~ PRINT";
-  expected = "x";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) DE L(x) ~ DELL(x) ~ DDEL(x) ~ DEL() ~ DEL ~ PRINT";
+    char expected[] = "x";
+    RUN_TEST(input, expected);
+  }
 
   //Delete - delete '\0'
-  input = "ADD(x) ~ DEL('\0') ~ PRINT";
-  expected = "x";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ DEL('\0') ~ PRINT";
+    char expected[] = "x";
+    RUN_TEST(input, expected);
+  }
 
   //Delete - delete two chars in same command
-  input = "ADD(ab) ~ DEL(ab) ~ PRINT";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(ab) ~ DEL(ab) ~ PRINT";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Delete - delete multiple elements
-  input = "ADD(x) ~ ADD(y) ~ ADD(x) ~ DEL(x) ~ PRINT";
-  expected = "y";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ ADD(y) ~ ADD(x) ~ DEL(x) ~ PRINT";
+    char expected[] = "y";
+    RUN_TEST(input, expected);
+  }
 
   //Delete - delete non existing element
-  input = "ADD(x) ~ DEL(y) ~ PRINT";
-  expected = "x";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ DEL(y) ~ PRINT";
+    char expected[] = "x";
+    RUN_TEST(input, expected);
+  }
 
   //Delete - delete empty list
-  input = "DEL(x) ~ PRINT";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "DEL(x) ~ PRINT";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Rev - malformed REV
-  input = "ADD(x) ~ R EV ~ RREV ~ REVV ~ REV(x) ~ PRINT";
-  expected = "x";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ R EV ~ RREV ~ REVV ~ REV(x) ~ PRINT";
+    char expected[] = "x";
+    RUN_TEST(input, expected);
+  }
 
   //Rev - rev empty list
-  input = "REV ~ PRINT";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "REV ~ PRINT";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Rev - rev one element
-  input = "ADD(x) ~ REV ~ PRINT";
-  expected = "x";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ REV ~ PRINT";
+    char expected[] = "x";
+    RUN_TEST(input, expected);
+  }
 
   //Rev - rev odd # of elements
-  input = "ADD(x) ~ ADD(y) ~ ADD(z) ~ REV ~ PRINT";
-  expected = "zyx";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ ADD(y) ~ ADD(z) ~ REV ~ PRINT";
+    char expected[] = "zyx";
+    RUN_TEST(input, expected);
+  }
 
   //Rev - rev even # of elements
-  input = "ADD(x) ~ ADD(y) ~ ADD(z) ~ ADD(w) ~ REV ~ PRINT";
-  expected = "wzyx";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ ADD(y) ~ ADD(z) ~ ADD(w) ~ REV ~ PRINT";
+    char expected[] = "wzyx";
+    RUN_TEST(input, expected);
+  }
 
   //Rev - rev twice in a row
-  input = "ADD(x) ~ ADD(y) ~ ADD(z) ~ ADD(w) ~ REV ~ REV ~ PRINT";
-  expected = "wzyx";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ ADD(y) ~ ADD(z) ~ ADD(w) ~ REV ~ REV ~ PRINT";
+    char expected[] = "wzyx";
+    RUN_TEST(input, expected);
+  }
 
   //Print - malformed PRINT
-  input = "ADD(x) ~ P RINT ~ PPRINT ~ PRINTT ~ PRINT()";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ P RINT ~ PPRINT ~ PRINTT ~ PRINT()";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Print - print empty list
-  input = "PRINT";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "PRINT";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Print - print 1 element
-  input = "ADD(x) ~ PRINT";
-  expected = "x";
+  {
+    char input[] = "ADD(x) ~ PRINT";
+    char expected[] = "x";
+    RUN_TEST(input, expected);
+  }
 
   //Print - print twice in a row
-  input = "ADD(x) ~ PRINT ~ PRINT";
-  expected = "x";
+  {
+    char input[] = "ADD(x) ~ PRINT ~ PRINT";
+    char expected[] = "x";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - malformed SORT
-  input = "ADD(4) ~ ADD(3) ~ S ORT ~ SSORT ~ SORTT ~ SORT() ~ PRINT";
-  expected = "43";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(4) ~ ADD(3) ~ S ORT ~ SSORT ~ SORTT ~ SORT() ~ PRINT";
+    char expected[] = "43";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - sort empty list
-  input = "SORT ~ PRINT";
-  expected = "";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "SORT ~ PRINT";
+    char expected[] = "";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - sort 1 element
-  input = "ADD(x) ~ SORT ~ PRINT";
-  expected = "x";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(x) ~ SORT ~ PRINT";
+    char expected[] = "x";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - sort odd # of elements
-  input = "ADD(z) ~ ADD(y) ~ ADD(x) ~ SORT ~ PRINT";
-  expected = "xyz";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(z) ~ ADD(y) ~ ADD(x) ~ SORT ~ PRINT";
+    char expected[] = "xyz";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - sort even # of elements
-  input = "ADD(1) ~ ADD(4) ~ ADD(3) ~ ADD(2) ~ SORT ~ PRINT";
-  expected = "1234";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(1) ~ ADD(4) ~ ADD(3) ~ ADD(2) ~ SORT ~ PRINT";
+    char expected[] = "1234";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - sort already sorted
-  input = "ADD(1) ~ ADD(2) ~ ADD(3) ~ ADD(4) ~ SORT ~ PRINT";
-  expected = "1234";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(1) ~ ADD(2) ~ ADD(3) ~ ADD(4) ~ SORT ~ PRINT";
+    char expected[] = "1234";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - sort list of same elements
-  input = "ADD(1) ~ ADD(1) ~ ADD(1) ~ ADD(1) ~ SORT ~ PRINT";
-  expected = "1111";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(1) ~ ADD(1) ~ ADD(1) ~ ADD(1) ~ SORT ~ PRINT";
+    char expected[] = "1111";
+    RUN_TEST(input, expected);
+  }
 
   //Sort - sort twice in a row
-  input = "ADD(3) ~ ADD(2) ~ ADD(1) ~ ADD(4) ~ SORT ~ SORT ~ PRINT";
-  expected = "1234";
-  RUN_TEST(input, expected);
+  {
+    char input[] = "ADD(3) ~ ADD(2) ~ ADD(1) ~ ADD(4) ~ SORT ~ SORT ~ PRINT";
+    char expected[] = "1234";
+    RUN_TEST(input, expected);
+  }
 
   //TODO sdx test: malformed SDX: S DX ~ SSDX ~ SDXX ~ SDX()
   //TODO sdx test: sdx empty list
@@ -279,6 +360,7 @@ static uint8_t tokenize_input(char *input)
   {
     is_delim = (c == '~');
 
+    printf("input: %s\n", input);
     *input *= !is_delim; // if is_delim, multiply by 0 (set to '\0'), otherwise multiply by 1 (keep the character)
     n_commands += is_delim;
 
